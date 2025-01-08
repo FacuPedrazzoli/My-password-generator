fdd2973d738f9abed437b5356ed51ea38dbbb297
FacuPedrazzoli <pedrazzolifacundo@gmail.com>
2025-01-08 17:44:23 -0300

    mejorar la generación de contraseñas y ajustar el formato del código

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function generatePasswords() {
 function generateRandomPassword() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
     let password = '';
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i <= 15; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
-    }
+    } 
     return password;
 }
 
