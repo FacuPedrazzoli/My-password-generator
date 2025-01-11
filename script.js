da94f20d91854cd8e59e26d6429357f24f0b2ba6
FacuPedrazzoli <pedrazzolifacundo@gmail.com>
2025-01-11 13:54:03 -0300

    Cambios de diseño

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function generatePasswords() {
+let generatePasswords = () => {
     const password1 = generateRandomPassword();
     const password2 = generateRandomPassword();
-    document.getElementById('password1').textContent = password1;
-    document.getElementById('password2').textContent = password2;
+    document.getElementById("password1").textContent = password1;
+    document.getElementById("password2").textContent = password2;
 }
 
-function generateRandomPassword() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-    let password = '';
+let generateRandomPassword = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let password = "";
     for (let i = 0; i <= 15; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
@@ -15,9 +15,9 @@ function generateRandomPassword() {
     return password;
 }
 
-function copyToClipboard(elementId) {
+let copyToClipboard = (elementId) => {
     const text = document.getElementById(elementId).textContent;
     navigator.clipboard.writeText(text).then(() => {
-        alert('Password copied to clipboard!');
+        alert("Password copied to clipboard!");
     });
 }
